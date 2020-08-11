89115a6fde05b67240b6659c0c8521029af9ab58
bohdan <rikudo765@gmail.com>
2020-08-11 18:08:10 +0300

    skeleton

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,8 +6,9 @@ import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 
-	"github.com/foundation-13/gpr/pkg/log"
 	"github.com/foundation-13/gpr/pkg/api/content"
+	"github.com/foundation-13/gpr/pkg/api/user"
+	"github.com/foundation-13/gpr/pkg/log"
 )
 
 func main() {
@@ -21,6 +22,9 @@ func main() {
 	log.L.Info("api started")
 	m := content.NewManager()
 	content.Assemble(e, m)
+
+	userMan := user.NewManager()
+	user.Assemble(e, userMan)
 
 	e.GET("/", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]string{"status": "green"})
